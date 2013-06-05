05feacc3c317bc2a937e2924197b36d11daebc2d
yimanwa <yimanwa@gmail.com>
2013-06-05 16:17:20 +0800

    this is my first time to use github ,let;s try
    this project work for zigbee location

---
+++ Uart/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Uart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM8";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(14, 31);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(122, 21);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "13719375500";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "输入手机号:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(14, 88);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "选择串口：";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(209, 31);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(144, 77);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(207, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "输入提示语：";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(32, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "开启短信控制";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(218, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "初始化GSM模块";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 175);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Uart/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Uart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+            string[] ports = System.IO.Ports.SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+            
+        }
+        private void Form1_Leave(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+        }
+
+
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            System.Threading.Thread.Sleep(400);
+            string i = serialPort1.ReadExisting();
+            int a = analyze_date(i);
+            if (a == 1)
+            {
+                MessageBox.Show("123");
+            //打开LED1
+            };
+            if (a == 2) 
+            { 
+            //打开LED2
+            };
+            if (a == 3)
+            { 
+            //打开LED3
+            };
+            if (a == 4)
+            { 
+            //打开LED4
+            };
+            if (a == 5)
+            { 
+            //返回温度信息
+            };
+
+        }
+
+
+        /* send_msg(string phone_number, string message, int temp1,int temp2,int temp3,int temp4)
+         * 函数功能：通过AT指令发送信息到指定的手机
+         * 函数参数：phone_number接收信息的电话号码；
+         *           message为要发送的提示信息；
+         *           temp1~4为4个节点的温度
+         */
+        private void send_msg(string phone_number, string message, int temp1,int temp2,int temp3,int temp4)
+        {
+            message = message + "\r\n site 1 : " + temp1 + "degree\r\n site 2 :" + temp2 + "degree\r\n site 3 :" + temp3 + "degree\r\n site 4 :" + temp4 + "degree\r\n";
+            byte[] b;
+            b = Encoding.Default.GetBytes("AT+CMGS=\"" + phone_number + "\"\r\n" + message);
+            byte[] c = new byte[b.Length + 1];
+            b.CopyTo(c, 0);
+            c[b.Length] = 26;
+            serialPort1.Write(c, 0, c.Length);
+        }
+
+
+        /* ======================init_gprs()==============================
+         * 函数功能：通过AT指令初始化GSM设备
+         */
+        private void init_gprs()
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.Open();
+            }
+            send_command("ATI\r\n" + "AT+CMGF=1\r\n" + "AT+CMGD=1\r\n");
+        }
+
+
+        /* ===============send_command(string command)=====================
+         * 
+         * 函数功能：通过串口发送控制指令
+         * 函数参数：string类型的指令
+         */
+        private void send_command(string command)
+        {
+            byte[] a;
+            a = Encoding.Default.GetBytes(command);
+            serialPort1.Write(a,0,a.Length);
+        }
+
+
+        /* =============analyze_date(string i)=======================
+         * 
+         * 函数功能：分析接连接GSM模块的串口收到的数据 
+         * 函数参数：i为通过串口接收到的数据。
+         * 函数返回值：如果是数据表示的是控制信息则返回控制信号：“1、2、3、4、5”
+         * 如果是数据表示接收信息则通过串口发出读取信息的命令，返回0。
+         * 如果以上的都不是则不做任何处理，返回0。
+         */
+        private int analyze_date(string i)
+        {
+            //当串口收到的信息长度为17时，表示GSM模块有接收到信息
+            int a = 0;
+            string b="0";
+            if (i.Length == 17) send_command("AT+CMGR=1\r\n");
+
+            //当串口收到的信息长度为85或83时，表示通过串口读取到了信息
+            if (i.Length == 85) b = i.Substring(76, 1);
+            if (i.Length == 83) b = i.Substring(74, 1);
+            a = Convert.ToInt32(b);
+            if (a > 0 && a < 10) send_command("AT+CMGD=1\r\n");
+            return a;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text == "关闭短信控制")
+            {
+                button3.Text = "打开短信控制";
+
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                }
+            }
+            else
+            { 
+            try
+            {
+                if(serialPort1.IsOpen)
+                    {
+                        serialPort1.Close();
+                    }
+            
+                    //打开combo1选择的串口
+                    serialPort1.PortName =comboBox1.SelectedItem.ToString();
+                    serialPort1.Open();
+                    button3.Text = "关闭短信控制";
+                    init_gprs();        
+                }
+                catch
+                {
+                    MessageBox.Show("没发现次串口或串口已经在使用");
+                    button3.Text = "打开短信控制";
+                }            
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                init_gprs();
+            }
+            catch
+            {
+                MessageBox.Show("没发现次串口或串口已经在使用");
+            }
+            
+        }
+
+
+    }
+}
